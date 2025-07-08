0a325ed7590002666c543983290ac0f50133fe8b
jace-ys <jaceys.tan@gmail.com>
2025-07-09 00:33:31 +0100

    Update 09072025

--- setup.sh
+++ setup.sh
@@ -1,27 +1,14 @@
+#!/bin/bash
+
 set -ex
 
 # brew
-curl -L https://github.com/Homebrew/brew/releases/download/4.2.0/Homebrew-4.2.0.pkg > Homebrew.pkg \
-    && sudo installer -pkg Homebrew.pkg -target / \
-    && rm Homebrew.pkg
-
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew bundle
 
 # git
 ln -sf "$(pwd)/git/.gitconfig" "$HOME/.gitconfig"
-ln -sf "$(pwd)/git/.gitignore" "$HOME/.gitignore" \
-    && git config --global core.excludesfile "$HOME/.gitignore"
-
-git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX" \
-    && git config --global interactive.diffFilter "diff-so-fancy --patch"
-
-# vscode
-ln -sf "$(pwd)/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
-ln -sf "$(pwd)/vscode/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json"
-
-while read extension; do
-    code --install-extension $extension --force
-done < ./vscode/extensions.txt
+ln -sf "$(pwd)/git/.gitignore" "$HOME/.gitignore"
 
 # zsh
 ln -sf "$(pwd)/zsh/.zshrc" "$HOME/.zshrc"
@@ -30,12 +17,22 @@ ln -sf "$(pwd)/zsh/.zshenv" "$HOME/.zshenv"
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# ghostty
+ln -sf "$(pwd)/ghostty/config" "$HOME/.config/ghostty/config"
 
-# powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-ln -sf "$(pwd)/p10k/.p10k.zsh" "$HOME/.p10k.zsh"
+# starship
+ln -sf "$(pwd)/starship.toml" "$HOME/.config/starship.toml"
 
-# asdf
-ln -sf "$(pwd)/asdf/.tool-versions" "$HOME/.tool-versions"
+# mise
+ln -sf "$(pwd)/mise/config.toml" "$HOME/.config/mise/config.toml"
+
+# zed
+ln -sf "$(pwd)/zed/settings.json" "$HOME/.config/zed/settings.json"
+
+# vscode
+ln -sf "$(pwd)/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+ln -sf "$(pwd)/vscode/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json"
+
+while read extension; do
+    code --install-extension $extension --force
+done < ./vscode/extensions.txt
